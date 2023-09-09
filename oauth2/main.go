656d40b013ea83e3c9399b6e88983647c517f01e
sntkn <mail.tsurumi@gmail.com>
2023-09-09 17:03:14 +0900

    Add Gin FW

--- oauth2/main.go
+++ oauth2/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	// Ginルーターの初期化
+	r := gin.Default()
+
+	// GETリクエストを受け取るエンドポイントの定義
+	r.GET("/api/hello", func(c *gin.Context) {
+		// JSONレスポンスを返す
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	})
+
+	// サーバーをポート8080で起動
+	r.Run(":8080")
 }
