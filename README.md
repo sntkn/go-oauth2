138efca8cf0f733321090d3a38b9a6bd9063d5ae
sntkn <mail.tsurumi@gmail.com>
2023-09-16 21:19:01 +0900

    add create table

--- README.md
+++ README.md
@@ -8,37 +8,51 @@
 
 ## Table structure
 
+### users
+
+| name     | type   |
+| -------- | ------ |
+| id       | uuid   |
+| name     | string |
+| email    | string |
+| password | string |
+
 ### oauth2_clients
 
-| name | type   |
-| ---- | ------ |
-| id   | uuid   |
-| name | string |
+| name          | type   |
+| ------------- | ------ |
+| id            | uuid   |
+| name          | string |
+| redirect_uris | string |
 
 ### oauth2_codes
 
-| name      | type   |
-| --------- | ------ |
-| code      | string |
-| client_id | uuid   |
-| user_id   | uuid   |
+| name         | type      |
+| ------------ | --------- |
+| code         | string    |
+| client_id    | uuid      |
+| user_id      | uuid      |
+| scope        | string    |
+| redirect_uri | string    |
+| expires_at   | timestamp |
 
 ### oauth2_tokens
 
-| name         | type     |
-| ------------ | -------- |
-| client_id    | uuid     |
-| user_id      | uuid     |
-| access_token | string   |
-| expires      | datetime |
+| name         | type      |
+| ------------ | --------- |
+| access_token | string    |
+| client_id    | uuid      |
+| user_id      | uuid      |
+| scope        | string    |
+| expires_at   | timestamp |
 
 ### oauth2_refresh_tokens
 
-| name          | type     |
-| ------------- | -------- |
-| access_token  | string   |
-| refresh_token | string   |
-| expires       | datetime |
+| name          | type      |
+| ------------- | --------- |
+| access_token  | string    |
+| refresh_token | string    |
+| expires_at    | timestamp |
 
 ## Reference
 
--- docker/database/init/init.sql
+++ docker/database/init/init.sql
@@ -5,4 +5,61 @@ GRANT ALL PRIVILEGES ON DATABASE auth TO app;
 --ユーザーを切り替え
 \c auth
 --テーブルを作成
+
+-- users テーブル
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+-- oauth2_clients テーブル
+CREATE TABLE oauth2_clients (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    redirect_uris VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+-- oauth2_codes テーブル
+CREATE TABLE oauth2_codes (
+    code VARCHAR(255) PRIMARY KEY,
+    client_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    scope VARCHAR(255) NOT NULL,
+    redirect_uri VARCHAR(255) NOT NULL,
+    expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    FOREIGN KEY (client_id) REFERENCES oauth2_clients (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- oauth2_tokens テーブル
+CREATE TABLE oauth2_tokens (
+    access_token VARCHAR(255) PRIMARY KEY,
+    client_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    scope VARCHAR(255) NOT NULL,
+    expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    FOREIGN KEY (client_id) REFERENCES oauth2_clients (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- oauth2_refresh_tokens テーブル
+CREATE TABLE oauth2_refresh_tokens (
+    access_token VARCHAR(255) PRIMARY KEY,
+    refresh_token VARCHAR(255) NOT NULL,
+    expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    FOREIGN KEY (client_id) REFERENCES oauth2_clients (id),
+);
+
 --テーブルにデータを挿入
