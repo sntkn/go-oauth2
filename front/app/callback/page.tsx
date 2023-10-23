29d03fd2f3b3b19b299e72c2bcb14a1893039c47
sntkn <mail.tsurumi@gmail.com>
2023-10-23 22:50:58 +0900

    callback

---
+++ front/app/callback/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function Callback() {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    // クエリパラメータから認可コードを取得
+    const code = searchParams.get('code')
+
+    if (code) {
+      // 認可コードが取得できた場合、アクセストークンの取得リクエストを送信
+      fetch('/api/getAccessToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // アクセストークンの処理
+          console.log('Received access token:', data.access_token)
+        })
+        .catch((error) => {
+          console.error('Error fetching access token:', error)
+        })
+    } else {
+      console.error('Authorization code not found in query parameters.')
+    }
+  }, [])
+
+  return (
+    <div>
+      <p>Processing...</p>
+    </div>
+  )
+}
