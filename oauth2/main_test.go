e2bf2662a6b694b59540aec2e30bbfb0f0e7f43b
sntkn <mail.tsurumi@gmail.com>
2024-07-22 23:28:25 +0900

    test

--- oauth2/main_test.go
+++ oauth2/main_test.go
@@ -1,10 +1,14 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPing(t *testing.T) {
 	_, err := ping()
 	if err != nil {
 		t.Fatal(err)
 	}
+	fmt.Println("success")
 }
