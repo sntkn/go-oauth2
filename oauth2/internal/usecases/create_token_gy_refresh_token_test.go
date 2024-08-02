af151299ec0323c358161637b1f062c28bcf111f
sntkn <mail.tsurumi@gmail.com>
2024-08-02 20:42:41 +0900

    upd refresh token usecase test

--- oauth2/internal/usecases/create_token_gy_refresh_token_test.go
+++ oauth2/internal/usecases/create_token_gy_refresh_token_test.go
@@ -18,49 +18,47 @@ import (
 func TestCreateTokenByRefreshToken_Invoke(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 
-	mockRepo := &repository.OAuth2RepositoryMock{
-		FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
-			return repository.RefreshToken{
-				AccessToken:  "valid_access_token",
-				RefreshToken: "valid_refresh_token",
-				ExpiresAt:    time.Now().Add(1 * time.Hour),
-				CreatedAt:    time.Now(),
-				UpdatedAt:    time.Now(),
-			}, nil
-		},
-		FindTokenFunc: func(accessToken string) (repository.Token, error) {
-			return repository.Token{
-				AccessToken: "valid_access_token",
-				UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-				ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-				Scope:       "scope",
-				ExpiresAt:   time.Now().Add(1 * time.Hour),
-				CreatedAt:   time.Now(),
-				UpdatedAt:   time.Now(),
-			}, nil
-		},
-		RegisterTokenFunc: func(t *repository.Token) error {
-			return nil
-		},
-		RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
-			return nil
-		},
-		RevokeTokenFunc: func(accessToken string) error {
-			return nil
-		},
-		RevokeRefreshTokenFunc: func(refreshToken string) error {
-			return nil
-		},
-	}
-
-	u := &CreateTokenByRefreshToken{
-		db:  mockRepo,
-		cfg: &config.Config{},
-	}
-
 	t.Run("valid refresh token", func(t *testing.T) {
-		refreshToken := "valid_refresh_token"
+		mockRepo := &repository.OAuth2RepositoryMock{
+			FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
+				return repository.RefreshToken{
+					AccessToken:  "valid_access_token",
+					RefreshToken: "valid_refresh_token",
+					ExpiresAt:    time.Now().Add(1 * time.Hour),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+				}, nil
+			},
+			FindTokenFunc: func(accessToken string) (repository.Token, error) {
+				return repository.Token{
+					AccessToken: "valid_access_token",
+					UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					Scope:       "scope",
+					ExpiresAt:   time.Now().Add(1 * time.Hour),
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil
+			},
+			RegisterTokenFunc: func(t *repository.Token) error {
+				return nil
+			},
+			RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
+				return nil
+			},
+			RevokeTokenFunc: func(accessToken string) error {
+				return nil
+			},
+			RevokeRefreshTokenFunc: func(refreshToken string) error {
+				return nil
+			},
+		}
+		u := &CreateTokenByRefreshToken{
+			db:  mockRepo,
+			cfg: &config.Config{},
+		}
 
+		refreshToken := "valid_refresh_token"
 		c, _ := gin.CreateTestContext(nil)
 		authTokens, err := u.Invoke(c, refreshToken)
 
@@ -71,10 +69,40 @@ func TestCreateTokenByRefreshToken_Invoke(t *testing.T) {
 	})
 
 	t.Run("invalid refresh token", func(t *testing.T) {
-		refreshToken := "invalid_refresh_token"
-		mockRepo.FindValidRefreshTokenFunc = func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
-			return repository.RefreshToken{}, sql.ErrNoRows
+		mockRepo := &repository.OAuth2RepositoryMock{
+			FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
+				return repository.RefreshToken{}, sql.ErrNoRows
+			},
+			FindTokenFunc: func(accessToken string) (repository.Token, error) {
+				return repository.Token{
+					AccessToken: "valid_access_token",
+					UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					Scope:       "scope",
+					ExpiresAt:   time.Now().Add(1 * time.Hour),
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil
+			},
+			RegisterTokenFunc: func(t *repository.Token) error {
+				return nil
+			},
+			RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
+				return nil
+			},
+			RevokeTokenFunc: func(accessToken string) error {
+				return nil
+			},
+			RevokeRefreshTokenFunc: func(refreshToken string) error {
+				return nil
+			},
 		}
+		u := &CreateTokenByRefreshToken{
+			db:  mockRepo,
+			cfg: &config.Config{},
+		}
+
+		refreshToken := "invalid_refresh_token"
 
 		c, _ := gin.CreateTestContext(nil)
 		authTokens, err := u.Invoke(c, refreshToken)
@@ -86,6 +114,44 @@ func TestCreateTokenByRefreshToken_Invoke(t *testing.T) {
 	})
 
 	t.Run("database error on finding refresh token", func(t *testing.T) {
+		mockRepo := &repository.OAuth2RepositoryMock{
+			FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
+				return repository.RefreshToken{
+					AccessToken:  "valid_access_token",
+					RefreshToken: "valid_refresh_token",
+					ExpiresAt:    time.Now().Add(1 * time.Hour),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+				}, nil
+			},
+			FindTokenFunc: func(accessToken string) (repository.Token, error) {
+				return repository.Token{
+					AccessToken: "valid_access_token",
+					UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					Scope:       "scope",
+					ExpiresAt:   time.Now().Add(1 * time.Hour),
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil
+			},
+			RegisterTokenFunc: func(t *repository.Token) error {
+				return nil
+			},
+			RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
+				return nil
+			},
+			RevokeTokenFunc: func(accessToken string) error {
+				return nil
+			},
+			RevokeRefreshTokenFunc: func(refreshToken string) error {
+				return nil
+			},
+		}
+		u := &CreateTokenByRefreshToken{
+			db:  mockRepo,
+			cfg: &config.Config{},
+		}
 		refreshToken := "db_error_refresh_token"
 		mockRepo.FindValidRefreshTokenFunc = func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
 			return repository.RefreshToken{}, errors.New("db error")
@@ -101,6 +167,44 @@ func TestCreateTokenByRefreshToken_Invoke(t *testing.T) {
 	})
 
 	t.Run("invalid access token", func(t *testing.T) {
+		mockRepo := &repository.OAuth2RepositoryMock{
+			FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
+				return repository.RefreshToken{
+					AccessToken:  "valid_access_token",
+					RefreshToken: "valid_refresh_token",
+					ExpiresAt:    time.Now().Add(1 * time.Hour),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+				}, nil
+			},
+			FindTokenFunc: func(accessToken string) (repository.Token, error) {
+				return repository.Token{
+					AccessToken: "valid_access_token",
+					UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					Scope:       "scope",
+					ExpiresAt:   time.Now().Add(1 * time.Hour),
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil
+			},
+			RegisterTokenFunc: func(t *repository.Token) error {
+				return nil
+			},
+			RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
+				return nil
+			},
+			RevokeTokenFunc: func(accessToken string) error {
+				return nil
+			},
+			RevokeRefreshTokenFunc: func(refreshToken string) error {
+				return nil
+			},
+		}
+		u := &CreateTokenByRefreshToken{
+			db:  mockRepo,
+			cfg: &config.Config{},
+		}
 		refreshToken := "valid_refresh_token"
 
 		mockRepo.FindTokenFunc = func(accessToken string) (repository.Token, error) {
@@ -117,6 +221,44 @@ func TestCreateTokenByRefreshToken_Invoke(t *testing.T) {
 	})
 
 	t.Run("database error on finding access token", func(t *testing.T) {
+		mockRepo := &repository.OAuth2RepositoryMock{
+			FindValidRefreshTokenFunc: func(refreshToken string, expiresAt time.Time) (repository.RefreshToken, error) {
+				return repository.RefreshToken{
+					AccessToken:  "valid_access_token",
+					RefreshToken: "valid_refresh_token",
+					ExpiresAt:    time.Now().Add(1 * time.Hour),
+					CreatedAt:    time.Now(),
+					UpdatedAt:    time.Now(),
+				}, nil
+			},
+			FindTokenFunc: func(accessToken string) (repository.Token, error) {
+				return repository.Token{
+					AccessToken: "valid_access_token",
+					UserID:      uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					ClientID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+					Scope:       "scope",
+					ExpiresAt:   time.Now().Add(1 * time.Hour),
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				}, nil
+			},
+			RegisterTokenFunc: func(t *repository.Token) error {
+				return nil
+			},
+			RegisterRefreshTokenFunc: func(t *repository.RefreshToken) error {
+				return nil
+			},
+			RevokeTokenFunc: func(accessToken string) error {
+				return nil
+			},
+			RevokeRefreshTokenFunc: func(refreshToken string) error {
+				return nil
+			},
+		}
+		u := &CreateTokenByRefreshToken{
+			db:  mockRepo,
+			cfg: &config.Config{},
+		}
 		refreshToken := "valid_refresh_token"
 
 		mockRepo.FindTokenFunc = func(accessToken string) (repository.Token, error) {
