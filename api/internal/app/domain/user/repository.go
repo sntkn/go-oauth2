38c46b550cd7c737202859811174849c5dd397a4
sntkn <mail.tsurumi@gmail.com>
2024-08-21 22:19:55 +0900

    ddd like structure

---
+++ api/internal/app/domain/user/repository.go
@@ -0,0 +1,42 @@
+package user
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/sntkn/go-oauth2/api/internal/app/interfaces/db/query"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	query *query.Query
+	gorm  *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		query: query.Use(db),
+		gorm:  db,
+	}
+}
+
+func (r *Repository) FindByID(id string) (*User, error) {
+	userQuery := r.query.User
+	user, err := userQuery.Where(userQuery.ID.Eq(id)).First()
+
+	if err != nil {
+		log.Printf("Could not query user: %v", err)
+
+		return nil, err
+	}
+
+	userID, err := uuid.Parse(user.ID)
+	if err != nil {
+		return nil, err
+	}
+	user2 := &User{
+		ID: userID,
+	}
+
+	return user2, nil
+}
