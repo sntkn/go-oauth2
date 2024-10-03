4baf0ff471032e40a6bde0934cd55ae722244495
sntkn <mail.tsurumi@gmail.com>
2024-10-03 22:43:50 +0900

    fix authorize test

--- oauth2/internal/usecases/authorize_test.go
+++ oauth2/internal/usecases/authorize_test.go
@@ -2,14 +2,12 @@ package usecases
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/sntkn/go-oauth2/oauth2/internal/repository"
-	"github.com/sntkn/go-oauth2/oauth2/pkg/config"
 	"github.com/sntkn/go-oauth2/oauth2/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -21,12 +19,9 @@ func TestAuthorizeInvoke(t *testing.T) {
 
 	t.Run("successful invoke", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
 		clientID := "00000000-0000-0000-0000-000000000000"
 		redirectURI := "http://example.com"
 
-		cfg := &config.Config{}
 		db := &repository.OAuth2RepositoryMock{
 			FindClientByClientIDFunc: func(clientID string) (repository.Client, error) {
 				return repository.Client{
@@ -39,57 +34,48 @@ func TestAuthorizeInvoke(t *testing.T) {
 			},
 		}
 
-		authorize := NewAuthorize(cfg, db)
-		err := authorize.Invoke(c, clientID, redirectURI)
+		authorize := NewAuthorize(db)
+		err := authorize.Invoke(clientID, redirectURI)
 		require.NoError(t, err)
 	})
 
 	t.Run("missing to find client_id", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
 		clientID := "00000000-0000-0000-0000-000000000000"
 		redirectURI := "http://example.com"
 
-		cfg := &config.Config{}
 		db := &repository.OAuth2RepositoryMock{
 			FindClientByClientIDFunc: func(clientID string) (repository.Client, error) {
 				return repository.Client{}, &errors.UsecaseError{Code: http.StatusBadRequest}
 			},
 		}
 
-		authorize := NewAuthorize(cfg, db)
-		err := authorize.Invoke(c, clientID, redirectURI)
+		authorize := NewAuthorize(db)
+		err := authorize.Invoke(clientID, redirectURI)
 		require.Error(t, err)
 		assert.IsType(t, &errors.UsecaseError{}, err)
 	})
 
 	t.Run("database error", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
 		clientID := "00000000-0000-0000-0000-000000000000"
 		redirectURI := "http://example.com"
 
-		cfg := &config.Config{}
 		db := &repository.OAuth2RepositoryMock{
 			FindClientByClientIDFunc: func(clientID string) (repository.Client, error) {
 				return repository.Client{}, errors.New("internal error")
 			},
 		}
 
-		authorize := NewAuthorize(cfg, db)
-		err := authorize.Invoke(c, clientID, redirectURI)
+		authorize := NewAuthorize(db)
+		err := authorize.Invoke(clientID, redirectURI)
 		require.Error(t, err)
 		assert.Contains(t, err.Error(), "internal error")
 	})
 
 	t.Run("redirect_uri does not match", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
 
-		cfg := &config.Config{}
 		db := &repository.OAuth2RepositoryMock{
 			FindClientByClientIDFunc: func(clientID string) (repository.Client, error) {
 				return repository.Client{
@@ -104,8 +90,8 @@ func TestAuthorizeInvoke(t *testing.T) {
 		clientID := "00000000-0000-0000-0000-000000000000"
 		redirectURI := "http://example.com/not/match"
 
-		authorize := NewAuthorize(cfg, db)
-		err := authorize.Invoke(c, clientID, redirectURI)
+		authorize := NewAuthorize(db)
+		err := authorize.Invoke(clientID, redirectURI)
 		require.Error(t, err)
 		assert.IsType(t, &errors.UsecaseError{}, err)
 		assert.Contains(t, err.Error(), "redirect uri does not match")
