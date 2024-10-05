147339397310c8c0eec3f774760b5f7d4a47eeba
sntkn <mail.tsurumi@gmail.com>
2024-10-05 22:25:40 +0900

    add authorize test

--- oauth2/internal/controllers/auth/authorize_test.go
+++ oauth2/internal/controllers/auth/authorize_test.go
@@ -5,153 +5,240 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/sntkn/go-oauth2/oauth2/internal/repository"
 	"github.com/sntkn/go-oauth2/oauth2/internal/session"
-	"github.com/sntkn/go-oauth2/oauth2/pkg/config"
 	"github.com/sntkn/go-oauth2/oauth2/pkg/errors"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-type MockAuthorizeUsecase struct {
-	mock.Mock
-}
-
-func (m *MockAuthorizeUsecase) Invoke(c *gin.Context, clientID, redirectURI string) error {
-	args := m.Called(c)
-	return args.Error(0)
-}
-
-func TestAuthorizeHandler(t *testing.T) {
+func TestAuthorize(t *testing.T) {
 	t.Parallel()
 	// Ginのテストモードをセット
 	gin.SetMode(gin.TestMode)
 
-	// テスト用のルーターを作成
-	r := gin.Default()
-	r.LoadHTMLGlob("../../../templates/*") // HTMLテンプレートのパスを指定
+	t.Run("successful authorize", func(t *testing.T) {
+		t.Parallel()
 
-	r.Use(func(c *gin.Context) {
-		c.Set("db", &repository.OAuth2RepositoryMock{
-			FindClientByClientIDFunc: func(clientID string) (repository.Client, error) {
-				return repository.Client{
-					ID:           uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-					Name:         "client Name",
-					RedirectURIs: "http://example.com",
-					CreatedAt:    time.Now(),
-					UpdatedAt:    time.Now(),
-				}, nil
+		r := gin.Default()
+		r.LoadHTMLGlob("../../../templates/*")
+
+		handler := &AuthorizeHandler{
+			sessionManager: &session.SessionManagerMock{
+				NewSessionFunc: func(c *gin.Context) session.SessionClient {
+					return &session.SessionClientMock{
+						SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
+							return nil
+						},
+					}
+				},
 			},
-		})
-		c.Set("session", &session.SessionClientMock{
-			SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
-				return nil
+			uc: &AuthorizeUsecaseMock{
+				InvokeFunc: func(clientID string, redirectURI string) error {
+					return nil
+				},
 			},
-		})
-		c.Set("cfg", &config.Config{})
-		c.Next() // 次のミドルウェア/ハンドラへ
+		}
+
+		r.GET("/authorize", handler.Authorize)
+
+		url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
+
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusFound, w.Code)
 	})
 
-	// authorizeハンドラをセット
-	r.GET("/authorize", func(c *gin.Context) {
-		AuthorizeHandler(c)
+	invalidParams := []string{
+		"client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz",          // response_typeが欠けている
+		"response_type=code&scope=read&redirect_uri=http://example.com&state=xyz",                                      // client_idが欠けている
+		"response_type=code&client_id=00000000-0000-0000-0000-000000000000&redirect_uri=http://example.com&state=xyz",  // scopeが欠けている
+		"response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&state=xyz",                       // redirect_uriが欠けている
+		"response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com", // stateが欠けている
+	}
+
+	for _, params := range invalidParams {
+		t.Run("bad request with missing params", func(t *testing.T) {
+			t.Parallel()
+
+			r := gin.Default()
+			r.LoadHTMLGlob("../../../templates/*")
+
+			handler := &AuthorizeHandler{
+				sessionManager: &session.SessionManagerMock{
+					NewSessionFunc: func(c *gin.Context) session.SessionClient {
+						return &session.SessionClientMock{}
+					},
+				},
+				uc: &AuthorizeUsecaseMock{},
+			}
+
+			r.GET("/authorize", handler.Authorize)
+
+			url := "/authorize?" + params
+
+			req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+			require.NoError(t, err)
+
+			w := httptest.NewRecorder()
+
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t, http.StatusBadRequest, w.Code)
+		})
+	}
+
+	t.Run("bad request error usecase", func(t *testing.T) {
+		t.Parallel()
+
+		r := gin.Default()
+		r.LoadHTMLGlob("../../../templates/*")
+
+		handler := &AuthorizeHandler{
+			sessionManager: &session.SessionManagerMock{
+				NewSessionFunc: func(c *gin.Context) session.SessionClient {
+					return &session.SessionClientMock{
+						SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
+							return nil
+						},
+					}
+				},
+			},
+			uc: &AuthorizeUsecaseMock{
+				InvokeFunc: func(clientID string, redirectURI string) error {
+					return errors.NewUsecaseError(http.StatusBadRequest, "bad requeest")
+				},
+			},
+		}
+
+		r.GET("/authorize", handler.Authorize)
+
+		url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
+
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
 	})
 
-	// テスト用のHTTPリクエストとレスポンスレコーダを作成
-	url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
+	t.Run("internal serer error usecase", func(t *testing.T) {
+		t.Parallel()
 
-	ctx := context.Background()
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
-	require.NoError(t, err)
+		r := gin.Default()
+		r.LoadHTMLGlob("../../../templates/*")
 
-	// レスポンスを記録するためのレスポンスレコーダを作成
-	w := httptest.NewRecorder()
+		handler := &AuthorizeHandler{
+			sessionManager: &session.SessionManagerMock{
+				NewSessionFunc: func(c *gin.Context) session.SessionClient {
+					return &session.SessionClientMock{
+						SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
+							return nil
+						},
+					}
+				},
+			},
+			uc: &AuthorizeUsecaseMock{
+				InvokeFunc: func(clientID string, redirectURI string) error {
+					return errors.NewUsecaseError(http.StatusInternalServerError, "internal server error")
+				},
+			},
+		}
 
-	// リクエストをルーターに送信
-	r.ServeHTTP(w, req)
+		r.GET("/authorize", handler.Authorize)
 
-	// ステータスコードが302 StatusFoundであることを確認
-	assert.Equal(t, http.StatusFound, w.Code)
-}
+		url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
 
-func TestAuthorize(t *testing.T) {
-	t.Parallel()
-	gin.SetMode(gin.TestMode)
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("internal serer error", func(t *testing.T) {
+		t.Parallel()
+
+		r := gin.Default()
+		r.LoadHTMLGlob("../../../templates/*")
+
+		handler := &AuthorizeHandler{
+			sessionManager: &session.SessionManagerMock{
+				NewSessionFunc: func(c *gin.Context) session.SessionClient {
+					return &session.SessionClientMock{
+						SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
+							return nil
+						},
+					}
+				},
+			},
+			uc: &AuthorizeUsecaseMock{
+				InvokeFunc: func(clientID string, redirectURI string) error {
+					return errors.New("internal server error")
+				},
+			},
+		}
+
+		r.GET("/authorize", handler.Authorize)
+
+		url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
+
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+		require.NoError(t, err)
+
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
 
 	t.Run("successful authorize", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-		r.LoadHTMLGlob("../../../templates/*")
-		c.Request = httptest.NewRequest(http.MethodGet, "/?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz", http.NoBody)
 
-		s := &session.SessionClientMock{
-			SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
-				return nil
+		r := gin.Default()
+		r.LoadHTMLGlob("../../../templates/*")
+
+		handler := &AuthorizeHandler{
+			sessionManager: &session.SessionManagerMock{
+				NewSessionFunc: func(c *gin.Context) session.SessionClient {
+					return &session.SessionClientMock{
+						SetNamedSessionDataFunc: func(c *gin.Context, key string, t any) error {
+							return errors.New("internal server error")
+						},
+					}
+				},
+			},
+			uc: &AuthorizeUsecaseMock{
+				InvokeFunc: func(clientID string, redirectURI string) error {
+					return nil
+				},
 			},
 		}
-		mockUC := new(MockAuthorizeUsecase)
-		mockUC.On("Invoke", mock.Anything).Return(nil)
 
-		authorize(c, s, mockUC)
+		r.GET("/authorize", handler.Authorize)
 
-		assert.Equal(t, http.StatusFound, w.Code)
+		url := "/authorize?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz"
 
-		mockUC.AssertExpectations(t)
-	})
+		req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, http.NoBody)
+		require.NoError(t, err)
 
-	t.Run("bad request error(validation)", func(t *testing.T) {
-		t.Parallel()
 		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-		r.LoadHTMLGlob("../../../templates/*")
-		c.Request = httptest.NewRequest(http.MethodGet, "/", http.NoBody)
 
-		s := &session.SessionClientMock{}
-		mockUC := new(MockAuthorizeUsecase)
-
-		authorize(c, s, mockUC)
-
-		assert.Equal(t, http.StatusBadRequest, w.Code)
-		mockUC.AssertExpectations(t)
-	})
-
-	t.Run("bad request error(usecase)", func(t *testing.T) {
-		t.Parallel()
-		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-		r.LoadHTMLGlob("../../../templates/*")
-		c.Request = httptest.NewRequest(http.MethodGet, "/?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz", http.NoBody)
-
-		s := &session.SessionClientMock{}
-		mockUC := new(MockAuthorizeUsecase)
-		mockUC.On("Invoke", mock.Anything).Return(&errors.UsecaseError{Code: http.StatusBadRequest})
-
-		authorize(c, s, mockUC)
-
-		assert.Equal(t, http.StatusBadRequest, w.Code)
-		mockUC.AssertExpectations(t)
-	})
-
-	t.Run("internal server error", func(t *testing.T) {
-		t.Parallel()
-		w := httptest.NewRecorder()
-		c, r := gin.CreateTestContext(w)
-		r.LoadHTMLGlob("../../../templates/*")
-		c.Request = httptest.NewRequest(http.MethodGet, "/?response_type=code&client_id=00000000-0000-0000-0000-000000000000&scope=read&redirect_uri=http://example.com&state=xyz", http.NoBody)
-
-		s := &session.SessionClientMock{}
-		mockUC := new(MockAuthorizeUsecase)
-		mockUC.On("Invoke", mock.Anything).Return(errors.New("internal error"))
-
-		authorize(c, s, mockUC)
+		r.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
-		mockUC.AssertExpectations(t)
 	})
 }
