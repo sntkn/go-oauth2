90999091c9fa2e3448a288796a7b9a13761c3e9b
sntkn <mail.tsurumi@gmail.com>
2024-10-06 15:24:51 +0900

    fixed delete token usecase test

--- oauth2/internal/usecases/delete_token.go
+++ oauth2/internal/usecases/delete_token.go
@@ -2,9 +2,7 @@ package usecases
 
 import (
 	"net/http"
-	"strings"
 
-	"github.com/gin-gonic/gin"
 	"github.com/sntkn/go-oauth2/oauth2/internal/repository"
 	"github.com/sntkn/go-oauth2/oauth2/pkg/errors"
 )
@@ -19,17 +17,7 @@ func NewDeleteToken(db repository.OAuth2Repository) *DeleteToken {
 	}
 }
 
-func (u *DeleteToken) Invoke(c *gin.Context) error {
-	// "Authorization" ヘッダーを取得
-	authHeader := c.GetHeader("Authorization")
-
-	// "Authorization" ヘッダーが存在しない場合や、Bearer トークンでない場合はエラーを返す
-	if authHeader == "" {
-		return errors.NewUsecaseError(http.StatusUnauthorized, "Missing or empty Authorization header")
-	}
-
-	// "Bearer " のプレフィックスを取り除いてトークンを抽出
-	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+func (u *DeleteToken) Invoke(tokenStr string) error {
 
 	if err := u.db.RevokeToken(tokenStr); err != nil {
 		return errors.NewUsecaseError(http.StatusInternalServerError, err.Error())
