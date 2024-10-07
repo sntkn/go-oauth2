b4865d7fccad47b84b5f4d9b6ffb4a34ada071fd
sntkn <mail.tsurumi@gmail.com>
2024-10-07 22:27:40 +0900

    fixed get user usecase test

--- oauth2/internal/usecases/get_user.go
+++ oauth2/internal/usecases/get_user.go
@@ -2,11 +2,8 @@ package usecases
 
 import (
 	"net/http"
-	"strings"
 
-	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"github.com/sntkn/go-oauth2/oauth2/internal/accesstoken"
 	"github.com/sntkn/go-oauth2/oauth2/internal/repository"
 	"github.com/sntkn/go-oauth2/oauth2/pkg/errors"
 )
@@ -21,33 +18,8 @@ func NewGetUser(db repository.OAuth2Repository) *GetUser {
 	}
 }
 
-func (u *GetUser) Invoke(c *gin.Context) (repository.User, error) {
-	// "Authorization" ヘッダーを取得
-	authHeader := c.GetHeader("Authorization")
-
-	var user repository.User
-
-	// "Authorization" ヘッダーが存在しない場合や、Bearer トークンでない場合はエラーを返す
-	if authHeader == "" {
-		return user, errors.NewUsecaseError(http.StatusUnauthorized, "missing or empty authorization header")
-	}
-
-	// "Bearer " のプレフィックスを取り除いてトークンを抽出
-	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
-
-	claims, err := accesstoken.Parse(tokenStr)
-
-	if err != nil {
-		return user, errors.NewUsecaseError(http.StatusUnauthorized, err.Error())
-	}
-
-	// TODO: find user
-	userID, err := uuid.Parse(claims.UserID)
-	if err != nil {
-		return user, errors.NewUsecaseError(http.StatusUnauthorized, err.Error())
-	}
-
-	user, err = u.db.FindUser(userID)
+func (u *GetUser) Invoke(userID uuid.UUID) (repository.User, error) {
+	user, err := u.db.FindUser(userID)
 	if err != nil {
 		return user, errors.NewUsecaseError(http.StatusUnauthorized, err.Error())
 	}
--- oauth2/internal/usecases/get_user_test.go
+++ oauth2/internal/usecases/get_user_test.go
@@ -2,15 +2,12 @@ package usecases
 
 import (
 	"database/sql"
-	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"github.com/sntkn/go-oauth2/oauth2/internal/accesstoken"
 	"github.com/sntkn/go-oauth2/oauth2/internal/repository"
 	"github.com/sntkn/go-oauth2/oauth2/pkg/errors"
 	"github.com/stretchr/testify/assert"
@@ -23,25 +20,14 @@ func TestGetUserInvoke(t *testing.T) {
 
 	t.Run("successful get user", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		c.Request = httptest.NewRequest(http.MethodGet, "/", http.NoBody)
 
-		token, err := accesstoken.Generate(accesstoken.TokenParams{
-			UserID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			ClientID:  uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			Scope:     "read",
-			ExpiresAt: time.Now().Add(1 * time.Hour),
-		})
-		require.NoError(t, err)
-
-		c.Request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+		userID := uuid.MustParse("00000000-0000-0000-0000-000000000000")
 
 		mockRepo := &repository.OAuth2RepositoryMock{}
 		getUser := NewGetUser(mockRepo)
 		mockRepo.FindUserFunc = func(id uuid.UUID) (repository.User, error) {
 			return repository.User{
-				ID:        uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+				ID:        userID,
 				Name:      "client Name",
 				Email:     "test@example.com",
 				CreatedAt: time.Now(),
@@ -49,81 +35,16 @@ func TestGetUserInvoke(t *testing.T) {
 			}, nil
 		}
 
-		user, err := getUser.Invoke(c)
+		user, err := getUser.Invoke(userID)
 
 		require.NoError(t, err)
 		assert.Equal(t, "test@example.com", user.Email)
 	})
 
-	t.Run("missing authorization header", func(t *testing.T) {
-		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		c.Request = httptest.NewRequest(http.MethodGet, "/", http.NoBody)
-
-		mockRepo := &repository.OAuth2RepositoryMock{}
-		getUser := NewGetUser(mockRepo)
-		user, err := getUser.Invoke(c)
-
-		require.Error(t, err)
-		assert.IsType(t, &errors.UsecaseError{}, err)
-		assert.Equal(t, repository.User{}, user)
-		assert.Equal(t, http.StatusUnauthorized, err.(*errors.UsecaseError).Code)
-		assert.Equal(t, "Code: 401, Message: missing or empty authorization header", err.Error())
-	})
-
-	t.Run("missing token", func(t *testing.T) {
-		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		c.Request = httptest.NewRequest(http.MethodGet, "/", http.NoBody)
-
-		token, err := accesstoken.Generate(accesstoken.TokenParams{
-			UserID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			ClientID:  uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			Scope:     "read",
-			ExpiresAt: time.Now().Add(-1 * time.Hour),
-		})
-		require.NoError(t, err)
-
-		c.Request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-
-		mockRepo := &repository.OAuth2RepositoryMock{}
-		getUser := NewGetUser(mockRepo)
-		mockRepo.FindUserFunc = func(id uuid.UUID) (repository.User, error) {
-			return repository.User{
-				ID:        uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-				Name:      "client Name",
-				Email:     "test@example.com",
-				CreatedAt: time.Now(),
-				UpdatedAt: time.Now(),
-			}, nil
-		}
-
-		user, err := getUser.Invoke(c)
-
-		require.Error(t, err)
-		assert.IsType(t, &errors.UsecaseError{}, err)
-		assert.Equal(t, repository.User{}, user)
-		assert.Equal(t, http.StatusUnauthorized, err.(*errors.UsecaseError).Code)
-		assert.Contains(t, err.Error(), "token is expired by")
-	})
-
 	t.Run("missing find user", func(t *testing.T) {
 		t.Parallel()
-		w := httptest.NewRecorder()
-		c, _ := gin.CreateTestContext(w)
-		c.Request = httptest.NewRequest(http.MethodGet, "/", http.NoBody)
 
-		token, err := accesstoken.Generate(accesstoken.TokenParams{
-			UserID:    uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			ClientID:  uuid.MustParse("00000000-0000-0000-0000-000000000000"),
-			Scope:     "read",
-			ExpiresAt: time.Now().Add(1 * time.Hour),
-		})
-		require.NoError(t, err)
-
-		c.Request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+		userID := uuid.MustParse("00000000-0000-0000-0000-000000000000")
 
 		mockRepo := &repository.OAuth2RepositoryMock{}
 		getUser := NewGetUser(mockRepo)
@@ -131,7 +52,7 @@ func TestGetUserInvoke(t *testing.T) {
 			return repository.User{}, sql.ErrNoRows
 		}
 
-		user, err := getUser.Invoke(c)
+		user, err := getUser.Invoke(userID)
 
 		require.Error(t, err)
 		assert.IsType(t, &errors.UsecaseError{}, err)
